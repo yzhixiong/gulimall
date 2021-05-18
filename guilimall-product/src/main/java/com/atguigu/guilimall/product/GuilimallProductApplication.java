077ee9168596a98ee8fc47383fcdd79a75a09de3
zhixiong yang <1042746716@qq.com>
2021-05-18 23:52:03 +0800

    项目初始构建

---
+++ guilimall-product/src/main/java/com/atguigu/guilimall/product/GuilimallProductApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.guilimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuilimallProductApplication.class, args);
+    }
+
+}
